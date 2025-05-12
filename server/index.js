fe3723b2e2a777fe2ee3537948633f78988edc51
linor1 <linor.salhov@gmail.com>
2025-05-12 18:55:41 +0300

    deleted comments

--- server/index.js
+++ server/index.js
@@ -3,10 +3,7 @@ const multer = require('multer');
 const app = express();
 const cors=require('cors')
 const port = 5000;
-const fs = require('fs');
 const {parse} = require('csv-parse')
-const records = [];
-
 
 app.use(cors());
 const storage = multer.memoryStorage();
@@ -14,7 +11,7 @@ const upload = multer({ storage: storage });
 
 app.post('/upload', upload.single("file"), (req, res) => {
 	csvText = req.file.buffer.toString('utf-8');
-	parser = parse(csvText, {columns: false, delimiter: ','}, function(err, records) {
+	parse(csvText, {columns: true, delimiter: ','}, function(err, records) {
 		if (err) {
 			console.error(err);
 			return res.status(500).send('Error parsing CSV file');
