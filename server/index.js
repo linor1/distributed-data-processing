646974429bafed46e7e608c855d6b62195fd19a8
linor1 <linor.salhov@gmail.com>
2025-05-12 18:06:40 +0300

    Read the file data on the server

--- server/index.js
+++ server/index.js
@@ -3,17 +3,26 @@ const multer = require('multer');
 const app = express();
 const cors=require('cors')
 const port = 5000;
+const fs = require('fs');
+const {parse} = require('csv-parse')
+const records = [];
+
 
 app.use(cors());
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload',upload.single("file"), (req, res) => {
-	console.log("hello server");
-	console.log(req.file);
-	console.log(req.body);
-	res.json({ status: "success" });
+app.post('/upload', upload.single("file"), (req, res) => {
+	csvText = req.file.buffer.toString('utf-8');
+	parser = parse(csvText, {columns: false, delimiter: ','}, function(err, records) {
+		if (err) {
+			console.error(err);
+			return res.status(500).send('Error parsing CSV file');
+		}
+		res.json(records);
+	});
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
